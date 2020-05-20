31b9053eb9c78441844ae7297e942e73758c8f6d
Parsa Saraydarpour <parsasaray@icloud.com>
2020-05-19 18:36:56 -0700

    Second Commit

--- MusicViewController.m
+++ MusicViewController.m
@@ -6,13 +6,19 @@
     self = [super initWithFrame:CGRectMake(10, 10, 200, 50)];
 
     if (self) {
-        self.backgroundColor = [UIColor whiteColor];
+        self.backgroundColor = [UIColor clearColor];
         self.windowLevel = UIWindowLevelStatusBar + 10000;
         self.hidden = NO;
         self.layer.cornerRadius = 10;
 
         songLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 30, 10)];
-
+        songLabel.backgroundColor = [UIColor whiteColor];
+        songLabel.textColor = [UIColor blackColor];
+        songLabel.textColor = [UIColor whiteColor];
+        songLabel.text = [NSString stringWithFormat: @"SONG TEST TEST"];
+        songLabel.font = [UIFont fontWithName:@"Arial Rounded MT Bold" size:(36.0)];
+        songLabel.clipsToBounds = YES;
+        [self addSubview:songLabel];
     }
 
     return self;
